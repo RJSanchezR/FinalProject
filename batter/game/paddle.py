f5c1d1c13ec37f26b989727bfa8296294ad3a53f
RJSanchezR <san16071@byui.edu>
2021-11-25 22:02:16 -0700

    deleted

--- batter/game/paddle.py
+++
@@ -1,8 +0,0 @@
-from game.actor import Actor
-from game import constants
-class Paddle(Actor):
-    def __init__(self):
-        super().__init__()
-        self._width = constants.PADDLE_WIDTH
-        self._height = constants.PADDLE_HEIGHT
-        self._image = constants.IMAGE_PADDLE
