8fa0e6d1bc49c92ff9f4cfad0797e1226f7b4158
RJSanchezR <san16071@byui.edu>
2021-11-28 15:02:07 -0700

    Interactive menu created

---
+++ fish-run/game/menu.py
@@ -0,0 +1,23 @@
+import sys
+from game.point import Point
+import raylibpy
+
+class Menu:
+    def __init__(self, color):
+        """The class constructor.
+        
+        Args:
+            self (Menu): An instance of Menu.
+        """
+        self._color = color
+
+    # def 
+
+    # def draw_rectangle_lines(self, posX, posY, width, height, color):
+
+    #     posX = .get_position().get_x()
+    #     posY = .get_position().get_y()
+    #     width = .get_width()
+    #     height = .get_height()
+    #     color = raylibpy.RED
+
