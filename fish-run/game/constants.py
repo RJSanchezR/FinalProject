0b6cfaccbf16045e75cbce6399fed06fc94219c8
RJSanchezR <san16071@byui.edu>
2021-12-02 20:48:29 -0700

    Replace ball w life. Bckgrd add. FR and Speed chng

--- fish-run/game/constants.py
+++ fish-run/game/constants.py
@@ -2,7 +2,9 @@ import os
 
 MAX_X = 800
 MAX_Y = 600
-FRAME_RATE = 120
+FRAME_RATE = 30
+
+GRAVITY = 1
 
 DEFAULT_SQUARE_SIZE = 20
 DEFAULT_FONT_SIZE = 20
@@ -10,9 +12,10 @@ DEFAULT_TEXT_OFFSET = 4
 
 IMAGE_HOOK = os.path.join(os.getcwd(), "./fish-run/assets/hook.png")
 IMAGE_FISH = os.path.join(os.getcwd(), "./fish-run/assets/60x50.png")
-IMAGE_BALL = os.path.join(os.getcwd(), "./fish-run/assets/ball.png")
+IMAGE_LIFE = os.path.join(os.getcwd(), "./fish-run/assets/ball.png")
 IMAGE_GAME_OVER = os.path.join(os.getcwd(), "./fish-run/assets/game_over.png")
 IMAGE_BACKGROUND = os.path.join(os.getcwd(), "./fish-run/assets/background.png")
+IMAGE_BACKGROUND1 = os.path.join(os.getcwd(), "./fish-run/assets/background-bottom.png")
 
 SOUND_START = os.path.join(os.getcwd(), "./fish-run/assets/start.wav")
 SOUND_BOUNCE = os.path.join(os.getcwd(), "./fish-run/assets/boing.wav")
@@ -32,16 +35,19 @@ HOOK_HEIGHT = 300
 
 HOOK_SPACE = 1
 
-FISH_SPEED = 15
+FISH_SPEED = 1
 
 FISH_WIDTH = 96
 FISH_HEIGHT = 78
 
-BALL_WIDTH = 24
-BALL_HEIGHT = 24
+LIFE_WIDTH = 24
+LIFE_HEIGHT = 24
 
 GAME_OVER_WIDTH = 236
 GAME_OVER_HEIGHT = 236
 
 BACKGROUND_WIDTH = 800
 BACKGROUND_HEIGHT = 600
+
+BACKGROUND1_WIDTH = 800
+BACKGROUND1_HEIGHT = 200
