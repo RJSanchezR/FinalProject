542f08cbc28c5c0befa8517b3ece99bc93eeac94
RJSanchezR <san16071@byui.edu>
2021-12-02 20:46:01 -0700

    Removed attributes in-class

--- fish-run/game/background.py
+++ fish-run/game/background.py
@@ -3,6 +3,3 @@ from game import constants
 class Background(Actor):
     def __init__(self):
         super().__init__()
-        self._width = constants.BACKGROUND_WIDTH
-        self._height = constants.BACKGROUND_HEIGHT
-        self._image = constants.IMAGE_BACKGROUND
