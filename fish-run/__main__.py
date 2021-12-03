45e3c2c5f9b4702de367a44eec0fbcc637353f50
RJSanchezR <san16071@byui.edu>
2021-12-02 20:44:41 -0700

    Rearrange of classes. Addition of gravity_action.

--- fish-run/__main__.py
+++ fish-run/__main__.py
@@ -6,20 +6,21 @@ from game import constants
 from game.director import Director
 from game.actor import Actor
 from game.point import Point
-from game.draw_actors_action import DrawActorsAction
 from game.input_service import InputService
 from game.output_service import OutputService
 from game.physics_service import PhysicsService
 from game.audio_service import AudioService
+from game.background import Background
 from game.hook import Hook
 from game.fish import Fish
 from game.lives import Lives
 from game.game_over import GameOver
-from game.background import Background
+from game.draw_actors_action import DrawActorsAction
 from game.control_actors_action import ControlActorsAction
+from game.move_actors_action import MoveActorsAction
+from game.gravity_action import GravityAction
 from game.handle_collisions_action import HandleCollisionsAction
 from game.handle_off_screen_action import HandleOffScreenAction
-from game.move_actors_action import MoveActorsAction
 
 def main():
 
@@ -29,8 +30,21 @@ def main():
     cast["background"] = []
 
     background = Background()
+    background.set_image(constants.IMAGE_BACKGROUND)
+    background.set_width(constants.BACKGROUND_WIDTH)
+    background.set_height(constants.BACKGROUND_HEIGHT)
     background.set_position(Point(1, 1))
+    background.set_gravity(False)
     cast["background"].append(background)
+
+    background1 = Background()
+    background1.set_image(constants.IMAGE_BACKGROUND1)
+    background1.set_width(constants.BACKGROUND1_WIDTH)
+    background1.set_height(constants.BACKGROUND1_HEIGHT)
+    background1.set_position(Point(1, 400))
+    background1.set_velocity(Point(-1,0))
+    background1.set_gravity(False)
+    cast["background"].append(background1)
 
     cast["hooks"] = []
 
@@ -44,31 +58,35 @@ def main():
         hook.set_velocity(Point(-1,0))
         hook.set_width(constants.HOOK_WIDTH)
         hook.set_height(constants.HOOK_HEIGHT)
+        hook.set_gravity(False)
         cast["hooks"].append(hook)
 
     cast["fish"] = []
 
     fish = Fish()
     fish.set_position(Point((constants.MAX_X / 2) - 40, (constants.MAX_Y / 2) + 50))
+    fish.set_gravity(True)
     cast["fish"].append(fish)
 
     cast["lives"] = []
+    space = 90
 
-    axis_y = constants.MAX_Y - 90
-    for x in range(1, 2):
-        for y in range(1, 4):
+    for x in range(1):
+        for y in range(3):
             life = Lives()
-            life.set_image(constants.IMAGE_BALL)
-            life.set_position(Point(1, axis_y))
-            life.set_width(constants.BALL_WIDTH)
-            life.set_height(constants.BALL_HEIGHT)
+            life.set_image(constants.IMAGE_LIFE)
+            life.set_position(Point(1, (constants.MAX_Y - space)))
+            life.set_width(constants.LIFE_WIDTH)
+            life.set_height(constants.LIFE_HEIGHT)
+            life.set_gravity(False)
             cast["lives"].append(life)
-            axis_y = axis_y + 30
+            space = space - 30
 
     cast["game_over"] = []
 
     game_over = GameOver()
     game_over.set_position(Point(282, 182))
+    game_over.set_gravity(False)
     cast["game_over"].append(game_over)
 
 
@@ -80,13 +98,14 @@ def main():
     physics_service = PhysicsService()
     audio_service = AudioService()
     draw_actors_action = DrawActorsAction(output_service)
-    move_actors_action = MoveActorsAction()
-    handle_off_screen_action = HandleOffScreenAction()
     control_actors_action = ControlActorsAction(input_service)
+    move_actors_action = MoveActorsAction()
+    gravity_action = GravityAction()
     handle_collisions_action = HandleCollisionsAction(physics_service)
+    handle_off_screen_action = HandleOffScreenAction()
 
     script["input"] = [control_actors_action]
-    script["update"] = [move_actors_action, handle_off_screen_action, handle_collisions_action]
+    script["update"] = [move_actors_action, handle_off_screen_action, handle_collisions_action, gravity_action]
     script["output"] = [draw_actors_action]
 
 
