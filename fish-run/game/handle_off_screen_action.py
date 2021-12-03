e64c7a0c997763e10f1cc1add2f3ad9bbf99b7d9
RJSanchezR <san16071@byui.edu>
2021-12-02 20:54:04 -0700

    Added bottom and top limits

--- fish-run/game/handle_off_screen_action.py
+++ fish-run/game/handle_off_screen_action.py
@@ -21,13 +21,19 @@ class HandleOffScreenAction(Action):
         # if fish.get_position().is_top():
         #     fish.set_velocity(Point(fish.get_velocity().get_x(), fish.get_velocity().get_y() * -1))
 
-        # Limits of the screen side
+        # Limits of the screen
         if fish.get_position().get_x() >= constants.MAX_X - 96:
             fish.set_position(Point(constants.MAX_X - 96, fish.get_position().get_y()))
 
         if fish.get_position().get_x() <= 1:
             fish.set_position(Point(10, fish.get_position().get_y()))
-        
+
+        if fish.get_position().is_bottom():
+            fish.set_position(Point(fish.get_position().get_x(), constants.MAX_Y -79))
+
+        if fish.get_position().is_top():
+            fish.set_position(Point(fish.get_position().get_x(), 2))
+
         # Limit on the bottom and losing a life condition
         for life in lives:
             if fish.get_position().is_bottom():
