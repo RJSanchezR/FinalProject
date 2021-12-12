dee3dd04667d72647a2acd39a047b64dacc19abd
RJSanchezR <san16071@byui.edu>
2021-12-11 18:09:21 -0700

    Fixed problem with the new tuple off screen

--- fish-run/game/handle_off_screen_action.py
+++ fish-run/game/handle_off_screen_action.py
@@ -3,6 +3,7 @@ from game.point import Point
 from game import constants
 from game.action import Action
 from game.audio_service import AudioService
+from game.hook import Hook
 
 class HandleOffScreenAction(Action):
 
@@ -14,10 +15,34 @@ class HandleOffScreenAction(Action):
         """
         fish = cast["fish"][0]
         lives = cast["lives"]
+        hooks = cast["hooks"]
+        # rocks = cast["rocks"]
+        i = 0
+        for obstacle in hooks:
+            if i % 2 == 0:
+                if obstacle.get_position().get_x() == constants.MAX_X / 2:
+                    
+                    hook = Hook()
+                    cast["hooks"].extend(hook.create_hook())
 
-        # if fish.get_position().over_side_wall():
-        #     fish.set_velocity(Point(fish.get_velocity().get_x() * -1, fish.get_velocity().get_y()))
-        
+            if obstacle.get_position().get_x() <= 5:
+
+                cast["hooks"].remove(obstacle)
+                
+            i += 1
+
+
+        # for rock in rocks:
+
+        #     if rock.get_position().get_x() == constants.MAX_X / 2:
+                
+        #         rock = Hook()
+        #         rock.create_rock()
+        #         cast["rocks"].append(rock)
+
+        #     if rock.get_position().get_x() <= 5:
+        #         cast["rocks"].remove(rock)
+
         # if fish.get_position().is_top():
         #     fish.set_velocity(Point(fish.get_velocity().get_x(), fish.get_velocity().get_y() * -1))
 
@@ -38,10 +63,10 @@ class HandleOffScreenAction(Action):
         for life in lives:
             if fish.get_position().is_bottom():
                 
-        #         # Set the "new" fish at the starting position
-        #         fish.set_position(Point((constants.MAX_X / 2) - 40, (constants.MAX_Y - 80)))
+                # # Set the "new" fish at the starting position
+                # fish.set_position(Point((constants.MAX_X / 2) - 40, (constants.MAX_Y - 80)))
                 
-        #         audio_service = AudioService()
-        #         audio_service.play_sound(constants.SOUND_START)
+                # audio_service = AudioService()
+                # audio_service.play_sound(constants.SOUND_START)
                 
                 cast["lives"].remove(life)
